37c6b8b1f530582c1e30d5287e48debddd148ad1
minabilgin <minabilgin35@hotmail.com>
2017-11-01 13:46:15 +0300

    String Eklendi

---
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -0,0 +1,17 @@
+
+package javaapplication2;
+
+
+public class JavaApplication2 {
+
+    public static void main(String[] args) {
+        
+        int n = 243;
+        String s = "abc";
+        String hello = "hello";
+        int m = 324;
+        
+      
+    }
+    
+}
