47f80f3c9d1a713998eb61bf3cdda79ac77c168c
HarveyQ <33281587+HarveyQ@users.noreply.github.com>
2017-11-01 04:32:00 -0700

    string ve int eklendi

---
+++ onlineHospital/src/onlinehospital/OnlineHospital.java
@@ -0,0 +1,14 @@
+
+package onlinehospital;
+
+public class OnlineHospital {
+
+
+    public static void main(String[] args) {
+        
+        String s = "abc";
+        int h = 134;
+        
+    }
+    
+}
